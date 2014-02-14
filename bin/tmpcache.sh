9f8f2d5c1f8c9eaae914a385cb92f35d78c10125
Martin Kourim <martink@koura>
2014-02-14 17:40:27 +0100

    tmpcache

---
+++ bin/tmpcache.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# nothing to do when config file does not exist
+[ ! -e ~/.tmpcache ] && exit 0
+
+# nothing to do if cache already exists
+[ -h ~/.cache/.tmpcache ] && exit 0
+
+# remove dangling symlink
+rm -f ~/.cache/.tmpcache
+
+# create cache dir
+cachedir="$(mktemp -d /mnt/tmpfs/${LOGNAME:-$USER}-cache.XXXXXXX)"
+
+# create link to cache dir in use
+ln -s "$cachedir" ~/.cache/.tmpcache
+
+# create symlink for each directory defined in config file
+while read dir; do
+  mkdir -p "${cachedir}/${dir}"
+  rm -f ~/.cache/"$dir"
+  ln -s "${cachedir}/${dir}" ~/.cache/"$dir"
+done < ~/.tmpcache
